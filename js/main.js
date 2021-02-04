661b6c3ef10dd2a825056e37361318b09bb136d3
Lichobor <mihaylown@yandex.ru>
2021-02-04 16:51:22 +0300

    написал две функции

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const getRandomInRange = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInRange(1, 100)
+
+const getRandom = function() {
+  Math.random();
+}
+
+getRandom()
